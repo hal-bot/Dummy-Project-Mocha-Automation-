86cdf7ad3c4ba2bcb7de531f7218cdf0e720c0b0
ketantilve87 <ketan.tilve@slalom.com>
2018-07-10 19:36:17 +0100

    mesage

--- test/specs/login.js
+++ test/specs/login.js
@@ -9,7 +9,7 @@ describe('Login Page Test Cases for Application ', function()
 {
 // This method will be executed only if we trigger smoke job 
 //This method will be executed if we trigger regression job
-    describe('Verify Login Page of the application - Positive', function()
+    describe('Verify user logs inside the application with valid username/pwd and verify the success message on the page ', function()
     {
         it('@Smoke Verify The page' , function(){
             LoginPage.open();
@@ -18,13 +18,14 @@ describe('Login Page Test Cases for Application ', function()
             LoginPage.submit();
             browser.pause(3000);
             assert.equal(LoginPage.verifyPositiveText,"You're logged in!");
-            reporter.feature('Authentication- Positve=');
+            reporter.feature('Authentication');
+            
         
         })
     })
 
     
-    describe('Verify Login Page of the application - Negative', function()
+    describe('Verify user doesn’t log inside the application because of incorrect username/pwd and verify the failure message on the page ', function()
     {
         // This method will not be executed only if we trigger smoke job 
         //This method will be executed if we trigger regression job
